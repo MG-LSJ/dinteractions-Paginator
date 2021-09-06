7f730074a579195339ef241257c9077045c604f7
Toricane <73972068+Toricane@users.noreply.github.com>
2021-09-05 18:27:33 -0700

    Why was it 1.3.2 instead of 1.3.1? gonna keep it 1.3.2 instead of 1.3.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r",encoding="utf-8") as fh:
 
 setup(
     name="dinteractions_Paginator",
-    version="1.3.3",
+    version="1.3.2",
     description="Unofficial discord-interactions multi page embed handler",
     long_description=long_description,
     long_description_content_type="text/markdown",
