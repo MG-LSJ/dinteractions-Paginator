939370ee98a41b07659c2cc943f1ed1319fcab0b
Toricane <73972068+Toricane@users.noreply.github.com>
2021-12-27 13:51:52 -0800

    version up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dinteractions_Paginator",
-    version="2.0.0a3",
+    version="2.0.0a4",
     description="Unofficial discord-interactions multi page embed handler",
     long_description=long_description,
     long_description_content_type="text/markdown",
