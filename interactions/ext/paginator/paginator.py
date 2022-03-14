ba1f9730c9f1553739b25b944cdaa171300e0942
Toricane <73972068+Toricane@users.noreply.github.com>
2022-03-14 14:40:34 -0700

    feat: selectmenu and more

--- interactions/ext/paginator/paginator.py
+++ interactions/ext/paginator/paginator.py
@@ -1,7 +1,8 @@
 from asyncio import TimeoutError
 from enum import Enum
 from random import randint
-from typing import Dict, List, Optional, Union
+from typing import Dict, List, Optional, Union, Callable, Coroutine
+from inspect import iscoroutinefunction
 
 from interactions.ext.wait_for import setup, wait_for_component
 
@@ -16,7 +17,10 @@ from interactions import (
     Embed,
     Emoji,
     Message,
+    SelectOption,
+    SelectMenu,
 )
+from .errors import StopPaginator
 
 
 class ButtonKind(str, Enum):
@@ -67,6 +71,9 @@ class Paginator(DictSerializerMixin):
         "timeout",
         "author_only",
         "buttons",
+        "select_placeholder",
+        "func_before_edit",
+        "func_after_edit",
         "id",
         "component_ctx",
         "index",
@@ -79,7 +86,10 @@ class Paginator(DictSerializerMixin):
     pages: Union[List[str], Dict[str, Embed]]
     timeout: Optional[Union[int, float]]
     author_only: Optional[bool]
-    buttons: Optional[List[Button]]
+    buttons: Optional[List[ButtonConfig]]
+    select_placeholder: Optional[str]
+    func_before_edit: Optional[Union[Callable, Coroutine]]
+    func_after_edit: Optional[Union[Callable, Coroutine]]
     id: int
     component_ctx: Optional[ComponentContext]
     index: int
@@ -94,7 +104,10 @@ class Paginator(DictSerializerMixin):
         pages: Union[List[Embed], List[str], Dict[str, Embed]],
         timeout: Optional[Union[int, float]] = None,
         author_only: Optional[bool] = False,
-        buttons: Optional[List[Button]] = None,
+        buttons: Optional[List[ButtonConfig]] = None,
+        select_placeholder: Optional[str] = "Page",
+        func_before_edit: Optional[Union[Callable, Coroutine]] = None,
+        func_after_edit: Optional[Union[Callable, Coroutine]] = None,
         **kwargs,
     ) -> None:
         if not hasattr(client, "wait_for_component"):
@@ -107,25 +120,31 @@ class Paginator(DictSerializerMixin):
             timeout=timeout,
             author_only=author_only,
             buttons={} if buttons is None else buttons,
+            select_placeholder=select_placeholder,
+            func_before_edit=func_before_edit,
+            func_after_edit=func_after_edit,
             **kwargs,
         )
-        self.id: int = kwargs.get("id", randint(0, 999_999))
-        self.component_ctx: Optional[ComponentContext] = None
+        self.id: int = kwargs.get("id", randint(0, 999_999_999))
+        self.component_ctx: Optional[ComponentContext] = kwargs.get(
+            "component_ctx", None
+        )
         self.index: int = kwargs.get("index", 0)
         self.top: int = kwargs.get("top", len(pages) - 1)
+        self.message: Message = kwargs.get("message", None)
         self.is_dict: bool = isinstance(pages, dict)
-        self.message: Message = None
 
         self._json.update(
             {
                 "id": self.id,
+                "component_ctx": self.component_ctx,
                 "index": self.index,
                 "top": self.top,
                 "message": self.message,
             }
         )
 
-    async def run(self):
+    async def run(self) -> Data:
         self.message = await self.send()
         while True:
             try:
@@ -136,19 +155,30 @@ class Paginator(DictSerializerMixin):
                     timeout=self.timeout,
                 )
             except TimeoutError:
-                await self.disable_components()
-                return Data(
-                    paginator=self,
-                    original_ctx=self.ctx,
-                    component_ctx=self.component_ctx,
-                    message=self.message,
-                )
+                await self.end_paginator()
+                return self.data()
+            result: Optional[bool] = None
+            if self.func_before_edit is not None:
+                try:
+                    result: Optional[bool] = await self.run_function()
+                except StopPaginator:
+                    return self.data()
+                if result is False:
+                    continue
             await self.component_logic()
             self.message = await self.edit()
+            if self.func_after_edit is not None:
+                try:
+                    result: Optional[bool] = await self.run_function()
+                except StopPaginator:
+                    return self.data()
+                if result is False:
+                    continue
 
     @property
     def custom_ids(self):
         return [
+            f"select{self.id}",
             f"first{self.id}",
             f"prev{self.id}",
             f"next{self.id}",
@@ -157,7 +187,12 @@ class Paginator(DictSerializerMixin):
 
     async def component_logic(self):
         custom_id: str = self.component_ctx.data.custom_id
-        if custom_id == f"first{self.id}":
+        print("custom_id", custom_id)
+        if custom_id == f"select{self.id}":
+            print("test")
+            print(self.component_ctx.data.values)
+            self.index = int(self.component_ctx.data.values[0]) - 1
+        elif custom_id == f"first{self.id}":
             self.index = 0
         elif custom_id == f"prev{self.id}":
             self.index -= 1
@@ -168,10 +203,38 @@ class Paginator(DictSerializerMixin):
         elif custom_id == f"last{self.id}":
             self.index = self.top
 
-    async def check(self, ctx) -> bool:
-        if self.author_only:
-            return self.component_ctx.user.id == self.ctx.user.id
-        return True
+    async def check(self, ctx: ComponentContext) -> bool:
+        return ctx.user.id == self.ctx.user.id if self.author_only else True
+
+    def select_row(self) -> ActionRow:
+        select_options = []
+        if self.is_dict:
+            for content, embed in self.pages.items():
+                content: str
+                embed: Embed
+                page_num: int = list(self.pages).index(content) + 1
+                title: Optional[str] = embed.title
+                label: str = ""
+                if not title:
+                    label = f'{page_num}: {f"{content[:93]}..." if len(content) > 96 else content}'
+                else:
+                    label = f'{page_num}: {f"{title[:93]}..." if len(title) > 96 else title}'
+                select_options.append(SelectOption(label=label, value=str(page_num)))
+        else:
+            for content in self.pages:
+                content: str
+                page_num: int = self.pages.index(content) + 1
+                label = f'{page_num}: {f"{content[:93]}..." if len(content) > 96 else content}'
+                select_options.append(SelectOption(label=label, value=str(page_num)))
+
+        select = SelectMenu(
+            options=select_options,
+            custom_id=f"select{self.id}",
+            placeholder=f"{self.select_placeholder} {self.index + 1}/{self.top + 1}",
+            min_values=1,
+            max_values=1,
+        )
+        return ActionRow(components=[select])
 
     def buttons_row(self) -> ActionRow:
         disabled_left = self.index == 0
@@ -219,7 +282,7 @@ class Paginator(DictSerializerMixin):
         )
 
     def components(self) -> List[ActionRow]:
-        return list(filter(None, [self.buttons_row()]))
+        return list(filter(None, [self.select_row(), self.buttons_row()]))
 
     async def send(self) -> Message:
         if self.is_dict:
@@ -247,7 +310,7 @@ class Paginator(DictSerializerMixin):
                 components=components if components is not None else self.components(),
             )
 
-    async def disable_components(self):
+    async def end_paginator(self):
         components = self.components()
         for action_row in components:
             for component in action_row.components:
@@ -255,3 +318,18 @@ class Paginator(DictSerializerMixin):
         # if not hasattr(self.message, "_client") or not self.message._client:
         #     self.message = Message(**self.message._json, _client=self.client._http)
         # await self.message.edit(components=components)
+
+    async def run_function(self, func) -> bool:
+        if func is not None:
+            if iscoroutinefunction(func):
+                return await func(self, self.component_ctx)
+            else:
+                return func(self, self.component_ctx)
+
+    def data(self):
+        return Data(
+            paginator=self,
+            original_ctx=self.ctx,
+            component_ctx=self.component_ctx,
+            message=self.message,
+        )
