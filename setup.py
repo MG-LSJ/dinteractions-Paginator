1f3b5b63723d6eabcf77d0641d0e6b48662c943a
Toricane <73972068+Toricane@users.noreply.github.com>
2022-03-23 16:08:06 -0700

    chore: update package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dinteractions_Paginator",
-    version="2.0.0a5",
+    version="2.0.0b1",
     description="Unofficial discord-interactions multi page embed handler",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -13,7 +13,7 @@ setup(
     author="Jugador123",
     author_email="momocordova@gmail.com",
     license="GNU",
-    packages=["dinteractions_Paginator"],
+    packages=["interactions.ext.paginator"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Development Status :: 5 - Production/Stable",
