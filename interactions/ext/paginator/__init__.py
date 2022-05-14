b77c82f4ed598f75c2f9b146d3b6fe035e194d0b
Toricane <prjwl028@gmail.com>
2022-05-14 15:19:06 -0700

    feat: improve __init__.py

--- interactions/ext/paginator/__init__.py
+++ interactions/ext/paginator/__init__.py
@@ -1,3 +1,14 @@
+"""
+dinteractions-Paginator
+
+- ButtonKind
+- Data
+- Page
+- Paginator
+- StopPaginator
+"""
+
+from .errors import StopPaginator
 from .paginator import ButtonKind, Data, Page, Paginator
 
-__all__ = ["ButtonKind", "Data", "Page", "Paginator"]
+__all__ = ["ButtonKind", "Data", "Page", "Paginator", "StopPaginator"]
